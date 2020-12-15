ea78078801277531104cd041f6c27fbf1096a099
Makraiyassin <makrai.yassin@gmail.com>
2020-12-15 16:32:08 +0100

    reglage erreurs dynamiques

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-const proxyurl = "https://cors-anywhere.herokuapp.com/";
+const proxyurl = "https://cors-anywhere.herokuapp.com/"; //indispensable pour l'API de Saba
 // const url = "https://becode-predict-ades.herokuapp.com/predict"; //API Saba
 // {"0":{"property-type":"APARTMENT","rooms-number":2,"zip-code":1000,"full-address":"","facades-number":2,"area":150}} // API Saba
 
@@ -25,7 +25,7 @@ document.querySelector("#go").addEventListener("click",function(event){
     let state = document.querySelector("#state").value;
 
     // console.log(area);
-    console.log(zipcode);
+    // console.log(zipcode);
     // console.log(rooms);
     // console.log(propertytype);
     // console.log(garden);
@@ -43,7 +43,7 @@ document.querySelector("#go").addEventListener("click",function(event){
     if (document.querySelector("#room").value == false) {
         document.querySelector("#divroom").insertAdjacentHTML("afterend",`<p class="error" style="color:red">veuillez remplir ce champ</p>`)
     }
-    if (document.querySelector("#area").value != false && document.querySelector("#zip").value != false && document.querySelector("#room").value != false && zipcode > 1000 && zipcode < 9999 ){
+    if (document.querySelector("#area").value != false && document.querySelector("#zip").value != false && document.querySelector("#room").value != false && zipcode >= 1000 && zipcode <= 9999 ){
 
         let info = {
             data:{
@@ -61,7 +61,7 @@ document.querySelector("#go").addEventListener("click",function(event){
             }
         }
         
-        fetch(proxyurl + url, {
+        fetch(url, {
             method: 'POST', 
             body: JSON.stringify(info), 
             headers: {'Content-Type': 'application/json'}
